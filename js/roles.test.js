52337147f3a61ac1e0afdf23cf639179e842a8c1
Zane Rockenbaugh <zane@liquid-labs.com>
2020-06-17 21:07:22 -0500

    updated test for json format
    removed no-longer relevant column match test [no ci]

--- js/roles.test.js
+++ js/roles.test.js
@@ -1,18 +1,14 @@
-import { RolesTsv } from '@liquid-labs/policies-model'
+/* global beforeAll describe expect test */
+import { Roles } from '@liquid-labs/policies-model'
 
-const rolesFile = `./policy/roles.tsv`
+const rolesFile = './policy/roles.json'
 
-describe(`roles.tsv`, () => {
-	let roles
-	beforeAll(() => { roles = new RolesTsv(rolesFile) })
+describe('roles.json', () => {
+  let roles
+  beforeAll(() => { roles = new Roles(rolesFile) })
 
-	test(`is successfully parsed`, () => {
-
-		expect(roles).toBeTruthy()
-		expect(roles.length).toBeGreaterThan(5)
-	})
-	test(`each line has a column for each key`, () => {
-		const expectedLength = roles.keys.length
-		roles.data.forEach((row, i) => expect(row.length).toEqual(expectedLength, `Expected ${expectedLength} but found ${row.length} columns at item ${i+1}`))
-	})
+  test('is successfully parsed', () => {
+    expect(roles).toBeTruthy()
+    expect(roles.getAll().length).toBeGreaterThan(5)
+  })
 })
