b108c40b105d3c03c506aa97d7fa5d8bbd440815
Zane Rockenbaugh <zane@liquid-labs.com>
2021-03-11 18:00:20 -0600

    hotfix: fix role refereneces; add policy change standards

--- js/roles.test.js
+++ js/roles.test.js
@@ -1,11 +1,13 @@
 /* global beforeAll describe expect test */
-import { Roles } from '@liquid-labs/orgs-model'
+import { Organization, Roles } from '@liquid-labs/orgs-model'
 
 const rolesFile = './policy/roles.json'
 
 describe('roles.json', () => {
   let roles
-  beforeAll(() => { roles = new Roles(rolesFile) })
+  beforeAll(() => {
+    roles = new Roles({}, rolesFile).hydrate()
+  })
 
   test('is successfully parsed', () => {
     expect(roles).toBeTruthy()
---
+++ policy/policy/standards/Policy Change Control Standards.md
@@ -0,0 +1,10 @@
+# Network Change Control Standards
+
+## Purpose and scope
+
+This document discusses the controls applied to any modifications to Company Policy.
+{{- template "change_control/standards/Common Change Control Scope" . }}
+
+## Standards
+
+{{ template "policy/standards/Policy Change Control Standards - items" . }}
