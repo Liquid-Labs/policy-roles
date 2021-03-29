de1f6912ad1af9c43c63856aeedb70ba686b31ff
Zane Rockenbaugh <zane@liquid-labs.com>
2021-03-28 21:54:13 -0500

    hotfix: remove debug

--- js/index.js
+++ js/index.js
@@ -2,8 +2,6 @@ import { genDoc } from './gen-doc'
 
 // TODO: the use of an 'index' file is a little misleading in this case. We want to build a tool, not a 'bundle'. We would like to update the catalyst-scripts (which need to be reworked) to be a bit more flexible.
 
-console.log('HI', process.argv)
-
 for (const line of genDoc(...process.argv.slice(2))) {
   console.log(line)
 }
