9b9e6a9e90043883253d014251e0bd4eee830eb9
Zane Rockenbaugh <zane@liquid-labs.com>
2021-06-12 15:57:45 -0500

    hotfix: note on previous implementaiton

--- bin/liq-gen-roles-ref.sh
+++ bin/liq-gen-roles-ref.sh
@@ -49,3 +49,8 @@ function real_path {
 }
 
 node $(dirname $(real_path $0))/gen-roles-ref.js "$@"
+
+# We were doing this, but it dosen't work with source maps...
+# cat <<'EOF' | node - "$@"
+# source ./gen-roles-ref.js # bash-rollup-no-recur
+# EOF
