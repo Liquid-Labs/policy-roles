02e195cc2a5a3550f6d6664e98ba453e45884d44
Zane Rockenbaugh <zane@liquid-labs.com>
2023-08-12 08:49:48 -0500

    updated test and deps

--- js/test/roles.test.js
+++ js/test/roles.test.js
@@ -1,15 +1,16 @@
 /* global beforeAll describe expect test */
-import * as fs from 'fs'
+import * as fs from 'node:fs'
+import * as fsPath from 'node:path'
 
-import { Roles } from '@liquid-labs/orgs-model'
+import { Roles } from '@liquid-labs/liq-roles'
 
-const rolesFile = './policy/roles.json'
+const rolesFile = fsPath.join(__dirname, '..', '..', 'policy', 'roles.json')
 
 // This is a bit idiomatic; we're actually testing the parse-ability of the `roles.json` file.
 describe('roles.json', () => {
   let roles
   beforeAll(() => {
-    roles = new Roles({ items : JSON.parse(fs.readFileSync(rolesFile)) })
+    roles = new Roles({ items : JSON.parse(fs.readFileSync(rolesFile)), org : {} })
   })
 
   test('is successfully parsed', () => {
